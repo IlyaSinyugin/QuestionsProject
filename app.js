2330394c7f6ea3093684b0c0b0aee2f432df6f9d
Ilya <ilya@sinyugin.ru>
2022-03-25 19:55:14 +0100

    selectors implemented

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-// traversing the dom 
-// multiple buttons -> querySelectorAll
-const btns = document.querySelectorAll('.question-btn');
+// // traversing the dom; Option 1
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const question = e.currentTarget.parentElement.parentElement;
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+// using selectors inside the element; Option 2
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    const btn = question.querySelector('.question-btn');
+    console.log(btn);
+    btn.addEventListener('click', function () {
         question.classList.toggle('show-text');
-    });
+    })
 });
